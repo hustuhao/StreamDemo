aae64f36f5dcc45ff5900ea03ac8114fc2144557
tuhao <tuhao@baijiahulian.com>
2020-11-16 09:33:51 +0800

    [feat]:sorted用例层架

--- src/main/java/stream/StreamDemo.java
+++ src/main/java/stream/StreamDemo.java
@@ -1,13 +1,13 @@
 package stream;
 
 import bo.StudentBO;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * @author tuhao
@@ -16,20 +16,15 @@ import java.util.stream.Stream;
  */
 public class StreamDemo {
 
-    @Test
-    public void testMapA() {
-        List <Integer> list = Arrays.asList(1,2,3,4,5);
-        // n = n * n
-        List<Integer> resutl = list.stream().map(n -> n * n).collect(Collectors.toList());
-        System.out.println(resutl);
-    }
+    private List<StudentBO> studentBOList = new ArrayList();
 
-    @Test
-    public void testMapB() {
+    @Before
+    public void initTest() {
         StudentBO studentA = new StudentBO();
         StudentBO studentB = new StudentBO();
         StudentBO studentC = new StudentBO();
         StudentBO studentD = new StudentBO();
+
         studentA.setId(1);
         studentA.setAge(20);
         studentA.setName("mama");
@@ -45,11 +40,23 @@ public class StreamDemo {
         studentC.setName("me");
         studentC.setScore(70);
 
-        List<StudentBO> studentBOList = new ArrayList();
         studentBOList.add(studentA);
         studentBOList.add(studentB);
         studentBOList.add(studentC);
         studentBOList.add(studentD);
+    }
+
+    @Test
+    public void testMapA() {
+        List <Integer> list = Arrays.asList(1,2,3,4,5);
+        // n = n * n
+        List<Integer> resutl = list.stream().map(n -> n * n).collect(Collectors.toList());
+        System.out.println(resutl);
+    }
+
+    @Test
+    public void testMapB() {
+
         //筛选id不为空的元素
         List<Integer> reuslt1 = studentBOList.stream().filter(studentBO -> studentBO.getId() != null).map(studentBO -> {
             Integer res = studentBO.getScore() * studentBO.getScore();
@@ -77,7 +84,19 @@ public class StreamDemo {
     @Test
     public void testDistinct() {
         List <Integer> list = Arrays.asList(1, 2, 2, 3, 4, 5, 5);
-        List<Integer> result = list.stream().distinct().collect(Collectors.toList());
+        List<Integer> result = list.stream().sorted().distinct().collect(Collectors.toList());
         System.out.println(result);
+    }
+
+    @Test
+    public void testSorted() {
+        // 年龄排序
+        List<StudentBO> result = studentBOList.stream()
+                .filter(studentBO -> studentBO.getId() != null)
+                .sorted((s1, s2) -> {
+                    return s1.getAge().compareTo(s2.getAge());
+                }).collect(Collectors.toList());
+        System.out.println(result);
+
     }
 }
